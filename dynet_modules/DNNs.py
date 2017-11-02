3d3c7f4341aa88a7df8e78499252b6843f1f9d4e
saikrishnarallabandi <srallaba@cs.cmu.edu>
2017-11-02 14:20:01 -0400

    Added DNNs

---
+++ dynet_modules/DNNs.py
@@ -0,0 +1,127 @@
+import dynet as dy
+import os
+import pickle
+import numpy as np
+
+debug = 1
+
+class FeedForwardNeuralNet(object):
+
+  def __init__(self, model, args):
+    self.pc = model.add_subcollection()
+    self.args = args
+    self.num_input = int(args[0])
+    self.num_output = int(args[2])
+    self.hidden_list = args[1]
+    self.act = args[3]
+    self.model = model
+    self.number_of_layers = len(self.hidden_list)
+    num_hidden_1 = self.hidden_list[0]
+    
+    # Add first layer
+    if debug == 1:
+        print "Adding input to first hidden layer weights ", num_hidden_1, self.num_input
+    self.W1 = self.pc.add_parameters((num_hidden_1, self.num_input))
+    self.b1 = self.pc.add_parameters((num_hidden_1))
+    
+    # Add remaining layers
+    self.weight_matrix_array = []
+    self.biases_array = []
+    self.weight_matrix_array.append(self.W1)
+    self.biases_array.append(self.b1)
+    for k in range(1, self.number_of_layers):
+              if debug == 1: 
+                   print "At ", k , " adding weights ", self.hidden_list[k], self.hidden_list[k-1]
+              self.weight_matrix_array.append(self.model.add_parameters((self.hidden_list[k], self.hidden_list[k-1])))
+              self.biases_array.append(self.model.add_parameters((self.hidden_list[k])))
+    if debug == 1:
+      print "Adding last hidden layer to output weights ", self.num_output, self.hidden_list[-1]
+    self.weight_matrix_array.append(self.model.add_parameters((self.num_output, self.hidden_list[-1])))
+    self.biases_array.append(self.model.add_parameters((self.num_output)))
+    self.spec = (self.num_input, self.hidden_list, self.num_output, self.act)
+   
+  def basic_affine(self, exp):
+    W1 = dy.parameter(self.W1)
+    b1 = dy.parameter(self.b1)
+    return dy.tanh(dy.affine_transform([b1,W1,exp]))
+
+  def calculate_loss(self, input, output):
+    weight_matrix_array = []
+    biases_array = []
+    for (W,b) in zip(self.weight_matrix_array, self.biases_array):
+         weight_matrix_array.append(dy.parameter(W))
+         biases_array.append(dy.parameter(b)) 
+    acts = self.act
+    w = weight_matrix_array[0]
+    b = biases_array[0]
+    act = acts[0]
+    intermediate = act(dy.affine_transform(b, w, input))
+    activations = [intermediate]
+    for (W,b,g) in zip(weight_matrix_array[1:], biases_array[1:], acts[1:]):
+        pred = g(dy.affine_transform([b, W, activations[-1]]))
+        activations.append(pred)  
+        losses = output - pred
+        return dy.l2_norm(losses)
+
+  def predict(self, input):
+    weight_matrix_array = []
+    biases_array = []
+    for (W,b) in zip(self.weight_matrix_array, self.biases_array):
+         weight_matrix_array.append(dy.parameter(W))
+         biases_array.append(dy.parameter(b))
+    g = self.act
+    w = weight_matrix_array[0]
+    b = biases_array[0]
+    intermediate = w*input + b
+    activations = [intermediate]
+    for (W,b) in zip(weight_matrix_array[1:], biases_array[1:]):
+        pred =  (W * g(activations[-1]))  + b
+        activations.append(pred)
+        return pred
+
+  # support saving:
+  def param_collection(self): return self.pc
+
+  @staticmethod
+  def from_spec(spec, model):
+    num_input, hidden_list, num_output, act = spec
+    return FeedForwardNeuralNet(model, [num_input, hidden_list, num_output, act])
+
+  '''
+  def save(self, path):
+     if debug:
+         print "Trying to save"
+     if not os.path.exists(path): os.makedirs(path)
+     dy.save(path + '_model', [self.model])
+     with open(path+"_model_params", "w") as f: pickle.dump(self.args, f)
+     if debug:
+        print "Saved to ", path
+
+  @staticmethod
+  def load(path, model,load_model_params=True):
+      if debug:
+         print "Trying to load the model"
+      if not os.path.exists(path): raise Exception("Model "+path+" does not exist")
+      with open(path+"_model_params", "r") as f: args = pickle.load(f)
+      [model] = dy.load(path + '_model', model)
+      return FeedForwardNeuralNet(m, args)
+  '''
+ 
+if debug:
+  # Load sample file and get first frame
+  a = np.loadtxt('VCC2SF1.frames')
+  A = a[0]
+  m = dy.Model()
+  dnn_1 = FeedForwardNeuralNet(m, [56, [2,12,15], 3, dy.rectify])
+  s = dnn_1.basic_affine(dy.inputTensor(A))
+  dy.save('models/basic_dnn', [dnn_1])
+  # Print a forward pass output
+  print "S:" , s.value()
+
+  # Reload the model
+  m2 = dy.Model()
+  [dnn_2] = dy.load('models/basic_dnn', m2)
+  b = dnn_2.basic_affine(dy.inputTensor(A))
+  print "B:" , b.value()
+
+  assert s.value() == b.value() 
